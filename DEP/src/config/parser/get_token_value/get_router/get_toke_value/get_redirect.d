25900677b4c7d05dde2866c5a1d2c2fa849805fc
Peru Riezu Muñoz <riezumunozperu@gmail.com>
2024-06-03 07:13:34 +0200

    progress in parser

--- DEP/src/config/parser/get_token_value/get_router/get_toke_value/get_redirect.d
+++
@@ -1,3 +0,0 @@
-./OBJ/src/config/parser/get_token_value/get_router/get_toke_value/get_redirect.o DEP/src/config/parser/get_token_value/get_router/get_toke_value/get_redirect.d : \
- src/config/parser/get_token_value/get_router/get_toke_value/get_redirect.cpp \
- /usr/include/stdc-predef.h
